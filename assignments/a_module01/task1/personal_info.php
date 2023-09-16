e086668b5b85fea7cf944082e8cee2bb8deb59d4
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-16 11:37:37 +0600

    1.1_task1_personal-information_done_module01

---
+++ assignments/a_module01/task1/personal_info.php
@@ -0,0 +1,12 @@
+<?php 
+$myName= "Md. Sohanur Rahman Sohan";
+$myAge= 27;
+$myCountry="Bangladesh";
+$myBriefIntro="My Name is {$myName}. I live in Dhaka,{$myCountry}. Now, I am {$myAge} years old. I have completed graduation in Economics & also 4 years Diploma in Computer Science. I love coding that's why I am trying to make myself a good software engineer.";
+echo "<h1>Personal Information Page</h1>";
+echo "<h2>My Name is: <span style='color:green;font-size:20px;'>{$myName}</span></h2>";
+echo "<h2>My Age is: <span style='color:green;font-size:20px;'>{$myAge}</span></h2>";
+echo "<h2>My Country is: <span style='color:green;font-size:20px;'>{$myCountry}</span></h2>";
+echo "<h2>A Brief Intro Of Mine: <span style='color:green;font-size:20px;'>{$myBriefIntro}</span></h2>";
+?>
+
